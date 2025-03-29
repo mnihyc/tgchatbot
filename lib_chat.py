ece844b9d8d8cd51844deb2b6995c4d111a4b818
mnihyc <rmnihyc@gmail.com>
2025-03-29 21:38:29 +0800

    Update google-genai SDK

--- lib_chat.py
+++ lib_chat.py
@@ -11,10 +11,12 @@ class LType(Enum):
 from typing import Callable, Dict, List
 from abc import abstractmethod
 
+from utils import detect_image
+
 class LChat:
     NAME = ''
-    MAX_RCTOKENS = 1048576 // 2
-    MAX_IMAGES = 3000 // 2
+    MAX_RCTOKENS = 8192
+    MAX_IMAGES = 0
     
     def __init__(self, cid: int, tid: int):
         self.history: List[Dict[str, object]] = []
@@ -22,16 +24,32 @@ class LChat:
         self.tid: int = tid
         self.a_m_c = None
 
-    @abstractmethod
-    async def add_message(self, parts: List[object], role: LRole) -> None:
-        pass
+    async def get_parts(self, message: str|List[str], type: LType) -> List[object]:
+        if type == LType.TEXT:
+            if isinstance(message, str):
+                message = [message]
+            return message
+        else:
+            res = []
+            for img in detect_image(message):
+                mime, data, _ = img
+                res.append({'mime_type': mime, 'data': data})
+            return res
 
     def add_message_callback(self, callback: Callable[[], None]) -> None:
         self.a_m_c = callback
-
-    @abstractmethod
-    async def get_parts(self, message: str|List[str], type: LType) -> List[object]:
-        pass
+    
+    async def add_message(self, parts: List[object], role: LRole) -> None:
+        match role:
+            case LRole.USER:
+                rolestr = 'user'
+            case LRole.SYSTEM:
+                rolestr = 'system'
+            case LRole.MODEL:
+                rolestr = 'model'
+        self.history.append({'role': rolestr, 'parts': parts})
+        if self.a_m_c:
+            self.a_m_c()
 
     async def pop_message(self, index: int = -1) -> Dict[str, object]:
         return self.history.pop(index)
@@ -40,10 +58,9 @@ class LChat:
         self.history.clear()
     
     async def rollback_message(self) -> bool:
+        await self.revoke_message()
         if len(self.history) > 1:
             self.history.pop()
-            if len(self.history) > 1:
-                self.history.pop()
             return True
         return False
 
@@ -53,12 +70,16 @@ class LChat:
             return True
         return False
     
-    @abstractmethod
     async def revoke_message(self) -> bool:
-        pass
+        while len(self.history) > 1:
+            if self.history[-1]['role'] == 'user':
+                self.history.pop()
+            else:
+                return True
+        return False
 
     @abstractmethod
-    async def cut_history(self, max_tokens: int = MAX_RCTOKENS, max_images: int = MAX_IMAGES) -> None:
+    async def cut_history(self, max_tokens: int = 0, max_images: int = 0) -> None:
         pass
     
     @abstractmethod
--- lib_gemini.py
+++ lib_gemini.py
@@ -7,18 +7,21 @@ assert API_KEY, 'GEMINI_API_KEY not set'
 API_KEY = [k.strip() for k in API_KEY.split() if k.strip()]
 
 from typing import AsyncIterator, Dict, List, override
-import google.generativeai as genai
+from google import genai
+from google.genai import types
 import random
-def select_api_key(api_key: str = None) -> None:
-    genai.configure(api_key=api_key or random.choice(API_KEY))
-select_api_key()
-print('Gemini Models: ', [m.name for m in genai.list_models()])
+def getclient() -> genai.Client:
+    client = genai.Client(api_key=random.choice(API_KEY))
+    return client
+print('Initialized Gemini Models: ', [m.name for m in getclient().models.list()])
 
-from utils import detect_image
-from lib_chat import LRole, LType, LChat
+import tiktoken
+from lib_chat import LChat
 
 class GeminiChat(LChat):
     NAME = 'gemini-2.0-flash'
+    MAX_RCTOKENS = 1048576
+    MAX_IMAGES = 3000
 
     def __init__(self, cid: int, tid: int):
         super().__init__(cid, tid)
@@ -26,53 +29,15 @@ class GeminiChat(LChat):
         self.top_k: int = 40
         self.top_p: float = 0.95
         self.max_tokens: int = 8192
-
-    async def get_parts(self, message: str|List[str], type: LType) -> List[object]:
-        if type == LType.TEXT:
-            if isinstance(message, str):
-                message = [message]
-            return message
-        else:
-            res = []
-            for img in detect_image(message):
-                mime, data, _ = img
-                res.append({'mime_type': mime, 'data': data})
-            return res
-    
-    async def add_message(self, parts: List[object], role: LRole) -> None:
-        match role:
-            case LRole.USER:
-                rolestr = 'user'
-            case LRole.SYSTEM:
-                rolestr = 'system'
-            case LRole.MODEL:
-                rolestr = 'model'
-        self.history.append({'role': rolestr, 'parts': parts})
-        if self.a_m_c:
-            self.a_m_c()
-    
-    @override
-    async def rollback_message(self) -> bool:
-        await self.revoke_message()
-        if len(self.history) > 1:
-            self.history.pop()
-            return True
-        return False
-    
-    async def revoke_message(self) -> bool:
-        while len(self.history) > 1:
-            if self.history[-1]['role'] == 'user':
-                self.history.pop()
-            else:
-                return True
-        return False
     
     async def count_tokens(self) -> int:
         #return sum(len(p) for m in self.history for p in m['parts'])
-        select_api_key()
-        return (await genai.GenerativeModel(self.NAME).count_tokens_async(self.history)).total_tokens
+        #return (await getclient().aio.models.count_tokens(model=self.NAME, contents=self.history)).total_tokens
+        encoding = tiktoken.get_encoding("cl100k_base")
+        return sum(len(encoding.encode(p))+1 if isinstance(p, str) else 258*4 for m in self.history for p in m['parts'])
 
-    async def cut_history(self, max_tokens: int = LChat.MAX_RCTOKENS, max_images: int = LChat.MAX_IMAGES) -> None:
+    async def cut_history(self, max_tokens: int = MAX_RCTOKENS, max_images: int = MAX_IMAGES) -> int:
+        cuts = len(self.history)
         imgs, i = 0, len(self.history)
         while i > 0:
             i -= 1
@@ -88,25 +53,53 @@ class GeminiChat(LChat):
                     continue
                 self.history.pop(i)
                 break
+        return cuts - len(self.history)
 
     async def ask(self) -> AsyncIterator[str]:
-        select_api_key()
-        model = genai.GenerativeModel(self.NAME,
-                    system_instruction=list(map(lambda x: x['parts'][0], filter(lambda x: x['role'] == 'system', self.history))) or None)
-        hist = list(filter(lambda x: x['role'] != 'system', self.history))
-        response = await model.generate_content_async(
-            hist,
-            generation_config=genai.types.GenerationConfig(
+        client = getclient()
+        hist = []
+        for c in list(filter(lambda x: x['role'] != 'system', self.history)):
+            role = c['role']
+            if role == 'system':
+                continue
+            parts = []
+            for p in c['parts']:
+                if isinstance(p, str):
+                    parts.append(types.Part.from_text(text=p))
+                elif isinstance(p, dict):
+                    parts.append(types.Part.from_bytes(mime_type=p['mime_type'], data=p['data']))
+            hist.append(types.Content(role=role, parts=parts))
+        text = ''
+        async for chunk in await client.aio.models.generate_content_stream(
+            model = self.NAME,
+            contents = hist,
+            config = genai.types.GenerateContentConfig(
+                system_instruction='\n'.join(list(map(lambda x: x['parts'][0], filter(lambda x: x['role'] == 'system', self.history)))) or None,
                 candidate_count=1,
                 temperature=self.temperature,
                 top_k=self.top_k,
                 top_p=self.top_p,
-                max_output_tokens=self.max_tokens),
-            safety_settings=dict.fromkeys(['harassment', 'hate', 'sex', 'danger'], 'block_none'),
-            stream=True
-        )
-        text = ''
-        async for chunk in response:
+                max_output_tokens=self.max_tokens,
+                safety_settings=[
+                    types.SafetySetting(
+                        category='HARM_CATEGORY_HATE_SPEECH',
+                        threshold='BLOCK_NONE',
+                    ),
+                    types.SafetySetting(
+                        category='HARM_CATEGORY_DANGEROUS_CONTENT',
+                        threshold='BLOCK_NONE',
+                    ),
+                    types.SafetySetting(
+                        category='HARM_CATEGORY_HARASSMENT',
+                        threshold='BLOCK_NONE',
+                    ),
+                    types.SafetySetting(
+                        category='HARM_CATEGORY_SEXUALLY_EXPLICIT',
+                        threshold='BLOCK_NONE',
+                    ),
+                ],
+            ),
+        ):
             text += chunk.text
             yield text
 
